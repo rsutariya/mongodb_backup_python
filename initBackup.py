875f6e33300c8c87022a52bc2d612e73cdae9a84
Naman Vyas <57315137+vyasn30@users.noreply.github.com>
2021-03-24 18:00:19 +0530

    Update initBackup.py

--- initBackup.py
+++ initBackup.py
@@ -1,22 +1,15 @@
 import os
 import sys
 import time
-#import boto3
-import argparse
+import schedule 
 
-parser = argparse.ArgumentParser(description= "For backing up mongodb from ec2 to s3")
-
-parser.add_argument("--minutes", metavar="m", type=int, help="Inteval in mins before backup")
-
-parser.add_argument("--s3loc", metavar="s", type=str, help="Destination s3 bucket")
-
-args = parser.parse_args()
+def job():
+        os.system("python3 execBackup.py")
 
 
+if __name__ == "__main__":
+    schedule.every(1).hour.do(job) #can work for mins as schedule.every(#mins).minutes.do(job)
 
-while 1:
-    if (time.time() % (args.minutes*60) == 0):
-        print("One minute passed, time to backup")
-        os.system("python3 testExecBackup.py \"backup.mongo.test\" ")
-    else:
-        continue
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
